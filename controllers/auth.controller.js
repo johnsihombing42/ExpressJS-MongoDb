dd3df1c6f678c8682fe491b741e37cd50e14ef68
johnsihombing42 <johnsihombing42@gmail.com>
2023-01-18 22:33:53 +0700

    add login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,6 +21,28 @@ module.exports = {
     }
   },
   login: async (req, res, next) => {
-    const { email, password } = req.body;
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user)
+        res.status(404).json({
+          status: false,
+          message: "User not found",
+        });
+
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword)
+        res.status(404).json({
+          status: false,
+          message: "password wrong",
+        });
+
+      return res.status(200).json({
+        status: true,
+        data: user,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../controllers");
 
 router.post("/register", controller.auth.register);
+router.post("/login", controller.auth.login);
 
 module.exports = router;

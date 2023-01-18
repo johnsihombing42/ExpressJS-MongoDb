53a04b22a465d855763aee1e2325c3d52d0034cd
johnsihombing42 <johnsihombing42@gmail.com>
2023-01-18 23:05:24 +0700

    update code

---
+++ controllers/user.controller.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+module.exports = {
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findOne({ id });
+      if (!user)
+        res.status(404).json({
+          status: false,
+          message: "User not found",
+        });
+
+      const userData = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json({
+        status: true,
+        message: "sucess update data",
+        data: userData,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  delete: async (req, res, next) => {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("./auth");
+const user = require("./user");
 
 router.use("/api/auth", auth);
+router.use("/api/user", user);
 
 module.exports = router;

04f130885231d7ad2d4aac0d1ac23253efa58512
johnsihombing42 <johnsihombing42@gmail.com>
2023-01-22 20:24:55 +0700

    update code

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,6 +25,37 @@ module.exports = {
   },
   delete: async (req, res, next) => {
     try {
+      const { id } = req.params;
+      const user = await User.findOne({ id });
+      if (!user)
+        res.status(404).json({
+          status: false,
+          message: "User not found",
+        });
+      await user.findByIdAndDelete({ id });
+      res.status(200).json({
+        status: true,
+        message: "Account has been deleted",
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+  read: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findById({ id });
+      const { password, updateAt, ...other } = user._doc;
+      if (!user)
+        res.status(404).json({
+          status: false,
+          message: "User not found",
+        });
+      res.status(200).json({
+        status: true,
+        message: "Account has been deleted",
+        data: other,
+      });
     } catch (err) {
       next(err);
     }

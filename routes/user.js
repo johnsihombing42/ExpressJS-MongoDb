e02f5eb48d8c5c629cea722045caa183787ac61b
johnsihombing42 <johnsihombing42@gmail.com>
2023-01-23 19:10:12 +0700

    update

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const controller = require("../controllers");
 
 router.put("/:id", controller.user.update);
-
+router.delete("/:id", controller.user.delete);
 module.exports = router;
